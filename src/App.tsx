a6bcb8a5ea88fb8ebbe1028b405f12c2bed3a9c9
tahakiziltepe <tahakiziltepe@hotmail.com>
2025-01-11 19:17:46 +0300

    2nd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreateOrderPage from './pages/CreateOrderPage';
 import TrackOrdersPage from './pages/TrackOrdersPage';
