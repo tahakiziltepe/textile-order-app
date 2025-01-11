a8e64c325818ce0f7ab05ec6f29eb9c4307d7581
tahakiziltepe <tahakiziltepe@hotmail.com>
2025-01-11 19:29:18 +0300

    asf

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/textile-order-app/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
