e1cea1e76d2af971cfe759b322d778bf70127d01
tahakiziltepe <tahakiziltepe@hotmail.com>
2025-01-11 19:45:34 +0300

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 }); 
